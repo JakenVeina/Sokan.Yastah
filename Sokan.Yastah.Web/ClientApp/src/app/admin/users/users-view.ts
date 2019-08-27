4e6dbb9d633dbeece8a923b3bfd6af5a2b97aef6
JakenVeina <jakenveina@yahoo.com>
2019-08-26 21:47:16 -0500

    Removed leftover debugging code from ClientApp UsersView

--- Sokan.Yastah.Web/ClientApp/src/app/admin/users/users-view.ts
+++ Sokan.Yastah.Web/ClientApp/src/app/admin/users/users-view.ts
@@ -17,26 +17,14 @@ export class UsersView {
     public constructor(
             appState: Store<IAppState>) {
         this.users = appState
-            .select(x => x.admin.users.overviews.map(x => ({
-                id: x.id,
-                username: x.username,
-                discriminator: x.discriminator,
-                firstSeen: x.firstSeen,
-                lastSeen: x.lastSeen
-            })));
+            .select(x => x.admin.users.overviews);
 
         appState.dispatch(new ReloadOverviewsAction());
     }
 
     public readonly users: Observable<IUserOverviewViewModel[]>;
 
-    public getId(user: IUserOverviewViewModel): number {
+    public getUserTrackingKey(user: IUserOverviewViewModel): number {
         return user.id;
-    }
-
-    public asdf(x: any, y: any): void {
-        x.isExpanded = y;
-        console.log(x);
-        console.log(y);
     }
 }
