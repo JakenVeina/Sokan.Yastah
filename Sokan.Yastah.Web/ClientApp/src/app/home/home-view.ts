292ebe10410d0cf5113a74001fd75c88db1201e2
JakenVeina <jakenveina@yahoo.com>
2019-08-26 23:41:01 -0500

    Removed unused imports.

--- Sokan.Yastah.Web/ClientApp/src/app/home/home-view.ts
+++ Sokan.Yastah.Web/ClientApp/src/app/home/home-view.ts
@@ -1,10 +1,7 @@
 import { Component } from "@angular/core";
-import { AuthenticationService } from "../authentication/authentication-service";
-import { AuthorizationService } from "../authorization/authorization-service";
 
 @Component({
-  selector: "home-view",
-  templateUrl: "./home-view.ts.html"
+    selector: "home-view",
+    templateUrl: "./home-view.ts.html"
 })
-export class HomeView {
-}
+export class HomeView { }
