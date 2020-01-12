cab67a55177f8db0a9bcf5b1b8d8132f0bfd813b
JakenVeina <jakenveina@yahoo.com>
2020-01-12 02:29:10 -0600

    Fixed a bug in Web/ClientApp/app/nav-menu/nav-menu-view. Seems like angular dropped support for property setter statements upon template variables, within inline template statements.

--- Sokan.Yastah.Web/ClientApp/src/app/nav-menu/nav-menu-view.ts
+++ Sokan.Yastah.Web/ClientApp/src/app/nav-menu/nav-menu-view.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-import { Observable } from "rxjs";
+
+import { BehaviorSubject, Observable } from "rxjs";
 
 import { AuthenticationService, AuthorizationService } from "../auth/services";
 
@@ -16,13 +17,22 @@ export class NavMenuView {
 
         this._authenticationService = authenticationService;
         this._authorizationService = authorizationService;
+
+        this._isAdminSubmenuVisible = new BehaviorSubject<boolean>(false);
+        this._isGameMasterSubmenuVisible = new BehaviorSubject<boolean>(false);
     }
 
     public get avatarUri(): Observable<string | null> {
         return this._authenticationService.observeAvatarUri;
     }
+    public get isAdminSubmenuVisible(): Observable<boolean> {
+        return this._isAdminSubmenuVisible;
+    }
     public get isAuthenticated(): Observable<boolean> {
         return this._authenticationService.observeIsAuthenticated;
+    }
+    public get isGameMasterSubmenuVisible(): Observable<boolean> {
+        return this._isGameMasterSubmenuVisible;
     }
     public get hasAdmin(): Observable<boolean> {
         return this._authorizationService.observeHasAdmin;
@@ -49,6 +59,17 @@ export class NavMenuView {
         return this._authenticationService.observeUsername;
     }
 
+    public toggleAdminSubmenu():
+            void {
+        this._isAdminSubmenuVisible.next(!this._isAdminSubmenuVisible.value);
+    }
+    public toggleGameMasterSubmenu():
+            void {
+        this._isGameMasterSubmenuVisible.next(!this._isGameMasterSubmenuVisible.value);
+    }
+
     private readonly _authenticationService: AuthenticationService;
     private readonly _authorizationService: AuthorizationService;
+    private readonly _isAdminSubmenuVisible: BehaviorSubject<boolean>;
+    private readonly _isGameMasterSubmenuVisible: BehaviorSubject<boolean>;
 }
