55befd2b693346b19ac14f0c020c58a1de8d1eea
JakenVeina <jakenveina@yahoo.com>
2020-03-17 19:22:51 -0500

    Disabled bogus compiler code suggestion.

--- Sokan.Yastah.Common/ApplicationLogEventType.cs
+++ Sokan.Yastah.Common/ApplicationLogEventType.cs
@@ -1,6 +1,9 @@
 ﻿namespace Sokan.Yastah.Common
 {
+    // Why the hell would this be a useful warning? Seriously. Just because I'm using hex? Go soak your head, this isn't a flags enum.
+    #pragma warning disable CA1027 // Mark enums with FlagsAttribute
     public enum ApplicationLogEventType
+    #pragma warning restore CA1027 // Mark enums with FlagsAttribute
     {
         Common      = 0x01000000,
         Data        = 0x02000000,
