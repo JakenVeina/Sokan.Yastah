c5e974348a8ebe3d16b2b92d2ff05c894f0f3b00
JakenVeina <jakenveina@yahoo.com>
2020-03-22 19:25:53 -0500

    Seriously, am I the only who who fucking knows what a Flags enum actually IS?!

---
+++ Sokan.Yastah.Data/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1027:Mark enums with FlagsAttribute",
+    Justification = "Just cause I'm using hexadecimal doesn't make this a flags enum, dipshits.",
+    Scope = "type",
+    Target = "~T:Sokan.Yastah.Data.Auditing.AuditingActionCategory")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1027:Mark enums with FlagsAttribute",
+    Justification = "Just cause I'm using hexadecimal doesn't make this a flags enum, dipshits.",
+    Scope = "type",
+    Target = "~T:Sokan.Yastah.Data.Permissions.PermissionCategory")]
